4a26c00108c3441896598f34d11947964e4bc33f
sourabh <sourabhkahar8140@gmail.com>
2025-03-21 17:25:49 +0530

    Added - New Portfolio Section

--- script/index.js
+++ script/index.js
@@ -209,3 +209,34 @@ var swiper = new Swiper(".testi-slider", {
 		clickable: true,
 	},
 });
+
+
+var swiper = new Swiper(".portfolio-slider", {
+	slidesPerView: 1,
+	spaceBetween: 10,
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+	breakpoints: {
+	  640: {
+		slidesPerView: 2,
+		spaceBetween: 20,
+	  },
+	  768: {
+		slidesPerView: 4,
+		spaceBetween: 40,
+	  },
+	  1024: {
+		slidesPerView: 5,
+		spaceBetween: 50,
+	  },
+	},
+  });
+
+  $('.gallery-item').magnificPopup({
+	type: 'image',
+	gallery:{
+	  enabled:true
+	}
+  });
